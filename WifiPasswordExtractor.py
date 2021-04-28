5cf3be44ed306851af537c09b3fd487d5a5d64a4
Vitalitty <59928922+Vitalitty@users.noreply.github.com>
2021-04-28 15:23:24 +0200

    else with detect lang

--- WifiPasswordExtractor.py
+++ WifiPasswordExtractor.py
@@ -1,19 +1,28 @@
+import locale
+import ctypes
 import subprocess
+
+windll = ctypes.windll.kernel32
+windll.GetUserDefaultUILanguage()
+lang = locale.windows_locale[windll.GetUserDefaultUILanguage()]
 
 data = subprocess.check_output(["netsh", "wlan", "show", "profiles"]).decode("ibm850").split("\n")
 
-wifis = [i.split(":")[1].strip('\r').strip() for i in data if "Profil Tous les utilisateurs" in i]
-
-if wifis:
+if lang == "fr_FR":
+    print("OS lang : fr_FR\n")
+    wifis = [i.split(":")[1].strip('\r').strip() for i in data if "Profil Tous les utilisateurs" in i]
     for wifi in wifis:
         data = subprocess.check_output(["netsh", "wlan", "show", "profile", wifi, "key=clear"]).decode("ibm850").split("\n")
         pw = [i.split(":")[1].strip('\r').strip() for i in data if "Contenu de la clé" in i]
         pw = pw[0] if pw else None
         print(("{:>20} : {}".format(wifi, pw)))
-else:
+elif lang == "en_US":
+    print("OS lang : en_US\n")
     wifis = [i.split(":")[1].strip('\r').strip() for i in data if "All User Profile" in i]
     for wifi in wifis:
         data = subprocess.check_output(["netsh", "wlan", "show", "profile", wifi, "key=clear"]).decode("ibm850").split("\n")
         pw = [i.split(":")[1].strip('\r').strip() for i in data if "Key Content" in i]
         pw = pw[0] if pw else None
         print(("{:>20} : {}".format(wifi, pw)))
+else:
+    print("Unknown OS")
