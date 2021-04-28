186946c9a18eb6377be537cda7e280e036424f90
Vitalitty <59928922+Vitalitty@users.noreply.github.com>
2021-04-28 12:59:26 +0200

    Fix for fr

--- WifiPasswordExtractor.py
+++ WifiPasswordExtractor.py
@@ -1,20 +1,11 @@
-import json
 import subprocess
 
-extracted = {}
-data = subprocess.check_output(["netsh", "wlan", "show", "profiles"]).decode("utf-8").split("\n")
-wifis = [i.split(":")[1].strip('\r').strip() for i in data if "All User Profile" in i]
+data = subprocess.check_output(["netsh", "wlan", "show", "profiles"]).decode("ibm850").split("\n")
+
+wifis = [i.split(":")[1].strip('\r').strip() for i in data if "Profil Tous les utilisateurs" in i]
 
 for wifi in wifis:
-    data = subprocess.check_output(["netsh", "wlan", "show", "profile", wifi, "key=clear"]).decode("utf-8").split("\n")
-    pw = [i.split(":")[1].strip('\r').strip() for i in data if "Key Content" in i]
+    data = subprocess.check_output(["netsh", "wlan", "show", "profile", wifi, "key=clear"]).decode("ibm850").split("\n")
+    pw = [i.split(":")[1].strip('\r').strip() for i in data if "Contenu de la clé" in i]
     pw = pw[0] if pw else None
-    extracted[wifi] = pw
-
-with open("Wifi-Passwords.json", 'w') as file:
-    file.write(json.dumps(extracted, sort_keys=True, indent=4))
-
-# =================================================================
-MAC = subprocess.check_output("ipconfig /all").decode("utf-8").replace("\r", '').replace("\n\n\n", '\n').strip()
-with open("MAC-Addresses.txt", "w") as file:
-    file.write(MAC)
+    print(("{:>20} : {}".format(wifi, pw)))
